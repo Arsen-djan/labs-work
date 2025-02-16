4308422438f697af36e430cd93dbc1f1f120c9f3
Arsen-djan <arseniyshred977@gmail.com>
2025-02-16 11:30:30 +0300

    Initial commit: CMake project setup

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+int main() {
+    int x = 5, y = 3;
+    std::cout << "Sum: " << add(x, y) << std::endl;
+    std::cout << "Difference: " << subtract(x, y) << std::endl;
+    return 0;
+}
